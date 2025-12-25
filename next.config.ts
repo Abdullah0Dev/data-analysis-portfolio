61404f5af593f1d396a576e92462af7fa484785a
Abdullah - Dev Minds <160670132+Abdullah0Dev@users.noreply.github.com>
2025-12-25 08:19:01 -0800

    Update next.config.ts to include new image source
    Added remote pattern for lpsonline.sas.upenn.edu

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    /* config options here lpsonline.sas.upenn.edu */
     reactCompiler: true,
     images: {
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "images.unsplash.com",
+            },
+              {
+                protocol: "https",
+                hostname: "lpsonline.sas.upenn.edu",
             },
             {
                 protocol: "https",
